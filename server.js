47f8013489ed5d8a637828149757b26d64f0e4f3
Jacob Zavita <jacobzavita@gmail.com>
2021-06-09 10:43:32 -0700

    add index.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 const express = require('express')
 const { join } = require('path')
 const axios = require('axios')
+const fs = require('fs')
 const app = express()
 
 // middleware
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.listen(3000)
